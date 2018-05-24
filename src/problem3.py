99de95138b9878d487c7e98f4424ceff3d5f4acd
mufichmj <mufichmj@rose-hulman.edu>
2018-05-24 16:04:58 -0400

    Mariah - problem3

--- src/problem3.py
+++ src/problem3.py
@@ -93,7 +93,7 @@ def problem3(point, circle1, circle2, window):
       :type window:    rg.RoseWindow
     """
     # ------------------------------------------------------------------
-    # TODO: 2. Implement and test this function.
+    # DONE: 2. Implement and test this function.
     #          Tests have been written for you (above).
     # ------------------------------------------------------------------
     point.attach_to(window)
