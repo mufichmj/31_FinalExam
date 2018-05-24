a74d84419afac0b3f028e016a305780e47c786e1
mufichmj <mufichmj@rose-hulman.edu>
2018-05-24 13:42:57 -0400

    Mariah - problem2

--- src/problem2.py
+++ src/problem2.py
@@ -2,9 +2,9 @@
 Final exam, problem 2.
 
 Authors: David Mutchler, Dave Fisher, Matt Boutell, Amanda Stouder,
-         their colleagues and PUT_YOUR_NAME_HERE.  May 2018.
+         their colleagues and Mariah Mufich.  May 2018.
 
-"""  # TODO: 1. PUT YOUR NAME IN THE ABOVE LINE.
+"""  # DONE: 1. PUT YOUR NAME IN THE ABOVE LINE.
 
 import time
 import testing_helper
@@ -19,9 +19,9 @@ def main():
     print()
     print('Un-comment the calls in MAIN one by one')
     print(' to run the testing code as you complete the TODOs.')
-    # run_test_problem2a()
-    # run_test_problem2b()
-    # run_test_problem2c()
+    run_test_problem2a()
+    run_test_problem2b()
+    run_test_problem2c()
 
 
 def is_prime(n):
@@ -173,7 +173,12 @@ def problem2a(x, sequence):
     # TODO: 2. Implement and test this function.
     #          Tests have been written for you (above).
     # -------------------------------------------------------------------------
+    new_list = []
+    for k in range(len(sequence)):
+        if sequence[k] > x:
+            new_list = new_list + [k]
 
+    return new_list
 
 def run_test_problem2b():
     """ Tests the  problem2b   function. """
